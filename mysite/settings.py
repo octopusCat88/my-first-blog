5c225a5a45d1c0d341533a9db79e0f3b60eede50
OctopusCat88 <apple.pies@hotmail.de>
2016-11-19 14:16:21 +0100

    Changed HTML for site.

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'gye6&&082l1g#rfpir8mfjdz3-0cs2*5r+ra8-fnw#+m96elai'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['muellermi.pythonanywhere.com']
+ALLOWED_HOSTS = ['muellermi.pythonanywhere.com', '127.0.0.1']
 
 
 # Application definition
