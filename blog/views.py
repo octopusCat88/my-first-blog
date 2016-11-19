c3282d2ceb2425eeb14aeefdef68e1459a97e261
OctopusCat88 <apple.pies@hotmail.de>
2016-11-19 15:47:28 +0100

    Modified templates to display all published posts from database plus time feature

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
+from django.utils import timezone
+from datetime import timedelta
+from .models import Post
 
 # Create your views here.
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    time = timezone.now() + timedelta(days = -24)
+    select = Post.objects.filter(published_date__lte=timezone.now())
+    posts = select.order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts, 'date': time})
+        # request = everything from user via internet
+        # 'blog/post_list.html' = temlate file
+        # {} = objects for template to use
